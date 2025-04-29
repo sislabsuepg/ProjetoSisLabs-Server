30d7556c719c9e900c9159844926be8509595303
GabrielBecher <gabriel.abecher@hotmail.com>
2025-04-29 16:21:47 -0300

    tratamento de erros interno

--- src/services/alunoService.ts
+++ src/services/alunoService.ts
@@ -1,4 +1,86 @@
 import Aluno from "../models/Aluno";
 
 export default class AlunoService {
+  static verificaRa(ra: string): string[] {
+    if (!ra) {
+      return ["RA do aluno é obrigatório"];
+    }
+    if (ra.length < 5 || ra.length > 13) {
+      return ["RA do aluno deve ter entre 5 e 13 caracteres"];
+    }
+    if (!/^[0-9]+$/.test(ra)) {
+      return ["RA do aluno deve conter apenas números"];
+    }
+    return [];
+  }
+
+  static verificaNome(nome: string): string[] {
+    if (!nome) {
+      return ["Nome do aluno é obrigatório"];
+    }
+    if (nome.length < 3 || nome.length > 40) {
+      return ["Nome do aluno deve ter entre 3 e 40 caracteres"];
+    }
+    if (!/^[a-zA-Z\u00C0-\u00FF ]+$/.test(nome)) {
+      return ["Nome do curso deve conter apenas letras"];
+    }
+    return [];
+  }
+
+  static verificaTelefone(telefone: string): string[] {
+    if (!/\(\d\d\) \d\d\d\d\d-\d\d\d\d/.test(telefone)) {
+      return ["Telefone inválido"];
+    }
+    return [];
+  }
+
+  static verificaEmail(email: string): string[] {
+    if (
+      !/[-A-Za-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/.test(
+        email
+      )
+    ) {
+      return ["Email inválido"];
+    }
+    return [];
+  }
+
+  static verificaSenha(senha: string): string[] {
+    if (!senha) {
+      return ["Senha do aluno é obrigatória"];
+    }
+    if (senha.length < 4 || senha.length > 6) {
+      return ["Senha do aluno deve ter entre 4 e 6 caracteres"];
+    }
+    if (!/^[0-9]+$/.test(senha)) {
+      return ["Senha do aluno deve conter apenas números"];
+    }
+    return [];
+  }
+
+  static async getAllAlunos() {
+    const alunos = await Aluno.findAll({
+      attributes: ["ra", "nome", "telefone", "ano", "email", "ativo"],
+      include: [
+        {
+          association: "curso",
+          attributes: ["id", "nome"],
+        },
+      ],
+    });
+
+    if (!alunos) {
+      return {
+        status: 404,
+        erros: ["Nenhum aluno encontrado"],
+        data: [],
+      };
+    }
+
+    return {
+      status: 200,
+      erros: [],
+      data: alunos,
+    };
+  }
 }
--- src/services/cursoService.ts
+++ src/services/cursoService.ts
@@ -1,149 +1,173 @@
 import Curso from "../models/Curso";
 
 export default class CursoService {
-  static verificaNome(nome: string) {
+  static verificaNome(nome: string): string[] {
     if (!nome) {
       return ["Nome do curso é obrigatório"];
     }
     if (nome.length < 3 || nome.length > 40) {
       return ["Nome do curso deve ter entre 3 e 40 caracteres"];
     }
-    if (!/^[a-zA-Z\u00C0-\u00FF ]+$/.test(nome)) {
-      return ["Nome do curso deve conter apenas letras e números"];
+    if (!/^[a-zA-Z\u00C0-\u00FF]+$/.test(nome)) {
+      return ["Nome do curso deve conter apenas letras"];
     }
     return [];
   }
 
   static async getAllCursos() {
-    const cursos = await Curso.findAll();
-    if (!cursos) {
+    try {
+      const cursos = await Curso.findAll();
+      if (!cursos) {
+        return {
+          status: 404,
+          erros: ["Nenhum curso encontrado"],
+          data: null,
+        };
+      }
       return {
-        status: 404,
-        erros: ["Nenhum curso encontrado"],
-        data: null,
+        status: 200,
+        erros: [],
+        data: cursos,
       };
+    } catch (e) {
+      return { status: 500, erros: ["Erro ao buscar cursos"], data: null };
     }
-    return {
-      status: 200,
-      erros: [],
-      data: cursos,
-    };
   }
 
   static async getCursoById(id: number) {
-    const curso = await Curso.findByPk(id);
-    if (!curso) {
+    try {
+      const curso = await Curso.findByPk(id);
+      if (!curso) {
+        return {
+          status: 404,
+          erros: ["Curso não encontrado"],
+          data: null,
+        };
+      }
       return {
-        status: 404,
-        erros: ["Curso não encontrado"],
-        data: null,
+        status: 200,
+        erros: [],
+        data: curso,
       };
+    } catch (e) {
+      return { status: 500, erros: ["Erro ao buscar curso"], data: null };
     }
-    return {
-      status: 200,
-      erros: [],
-      data: curso,
-    };
   }
 
   static async getCursoByNome(nome: string) {
-    const erros: string[] = this.verificaNome(nome);
-    if (erros.length > 0) {
+    try {
+      const erros: string[] = this.verificaNome(nome);
+      if (erros.length > 0) {
+        return {
+          status: 400,
+          erros: erros,
+          data: null,
+        };
+      }
+      const curso = await Curso.findOne({ where: { nome: nome } });
+      if (!curso) {
+        return {
+          status: 404,
+          erros: ["Curso não encontrado"],
+          data: null,
+        };
+      }
       return {
-        status: 400,
-        erros: erros,
-        data: null,
+        status: 200,
+        erros: [],
+        data: curso,
       };
+    } catch (e) {
+      return { status: 500, erros: ["Erro ao buscar curso"], data: null };
     }
-    const curso = await Curso.findOne({ where: { nome: nome } });
-    if (!curso) {
-      return {
-        status: 404,
-        erros: ["Curso não encontrado"],
-        data: null,
-      };
-    }
-    return {
-      status: 200,
-      erros: [],
-      data: curso,
-    };
   }
 
   static async createCurso(nome: string) {
-    const erros: string[] = this.verificaNome(nome);
-    if (erros.length > 0) {
-      return {
-        status: 400,
-        erros: erros,
-        data: null,
-      };
-    }
+    try {
+      const erros: string[] = this.verificaNome(nome);
+      if (erros.length > 0) {
+        return {
+          status: 400,
+          erros: erros,
+          data: null,
+        };
+      }
 
-    const existe = await Curso.findOne({ where: { nome: nome } });
-    if (existe) {
-      return {
-        status: 400,
-        erros: ["Curso já existe"],
-        data: null,
-      };
-    }
+      const existe = await Curso.findOne({ where: { nome: nome } });
+      if (existe) {
+        return {
+          status: 400,
+          erros: ["Curso já existe"],
+          data: null,
+        };
+      }
 
-    const curso = await Curso.create({ nome: nome });
-    if (!curso) {
+      const curso = await Curso.create({ nome: nome });
+      if (!curso) {
+        return {
+          status: 500,
+          erros: ["Erro ao criar curso"],
+          data: null,
+        };
+      }
       return {
-        status: 500,
-        erros: ["Erro ao criar curso"],
-        data: null,
+        status: 201,
+        erros: [],
+        data: curso,
       };
+    } catch (e) {
+      return { status: 500, erros: ["Erro ao criar curso"], data: null };
     }
-    return {
-      status: 201,
-      erros: [],
-      data: curso,
-    };
   }
 
   static async updateCurso(id: number, nome: string) {
-    const erros: string[] = this.verificaNome(nome);
-    if (erros.length > 0) {
+    try {
+      const erros: string[] = this.verificaNome(nome);
+      if (erros.length > 0) {
+        return {
+          status: 400,
+          erros: erros,
+          data: null,
+        };
+      }
+      const curso = await Curso.findByPk(id);
+      if (!curso) {
+        return {
+          status: 404,
+          erros: ["Curso não encontrado"],
+          data: null,
+        };
+      }
+      curso.nome = nome;
+      await curso.save();
       return {
-        status: 400,
-        erros: erros,
-        data: null,
+        status: 200,
+        erros: [],
+        data: curso,
       };
+    } catch (e) {
+      return { status: 500, erros: ["Erro ao atualizar curso"], data: null };
     }
-    const curso = await Curso.findByPk(id);
-    if (!curso) {
-      return {
-        status: 404,
-        erros: ["Curso não encontrado"],
-        data: null,
-      };
-    }
-    curso.nome = nome;
-    await curso.save();
-    return {
-      status: 200,
-      erros: [],
-      data: curso,
-    };
   }
 
   static async deleteCurso(id: number) {
-    const curso = await Curso.findByPk(id);
-    if (!curso) {
+    try {
+      const curso = await Curso.findByPk(id);
+      if (!curso) {
+        return {
+          status: 404,
+          erros: ["Curso não encontrado"],
+          data: null,
+        };
+      }
+      await curso.destroy();
       return {
-        status: 404,
-        erros: ["Curso não encontrado"],
+        status: 200,
+        erros: [],
         data: null,
       };
+    } catch (e) {
+      return { status: 500, erros: ["Erro ao deletar curso"], data: null };
     }
-    await curso.destroy();
-    return {
-      status: 200,
-      erros: [],
-      data: null,
-    };
   }
 }
