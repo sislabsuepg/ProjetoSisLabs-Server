0552fc1e8ba0830da410d2f95798e883b14432e5
Gabriel A. Becher <gabriel.abecher@hotmail.com>
2025-05-01 20:28:04 -0300

    wrong  typo

---
+++ src/routes/usuarioRoutes.ts
@@ -0,0 +1,17 @@
+import UsuarioController from "../controllers/UsuarioController";
+
+import { Router } from "express";
+
+const router: Router = Router();
+
+router.get("/", UsuarioController.index);
+
+router.get("/:id", UsuarioController.show);
+
+router.post("/", UsuarioController.store);
+
+router.put("/:id", UsuarioController.update);
+
+router.delete("/:id", UsuarioController.destroy);
+
+export default router;
